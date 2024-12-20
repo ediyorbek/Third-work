28c8f06f29db780e2a5e2386543e1826904a42e8
avocadok <avocadok@student.21-school.ru>
2024-12-20 23:35:33 +0500

    Initial commit

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,56 +4,56 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv('medical_examination.csv')
 
 # 2
-df['overweight'] = None
+df['bmi'] = (df['weight'] / (df['height'] ** 2)) * 100
+df['overweight'] = (df['bmi'] > 25).astype(int)
 
 # 3
-
+columns = ['ap_lo', 'cholesterol', 'gluc', 'smoke', 'alco', 'active', 'cardio', 'overweight']
+for column in columns:
+    df[column] = df[column].apply(lambda x: 1 if x > 1 else x)
 
 # 4
 def draw_cat_plot():
-    # 5
-    df_cat = None
 
+    # 5
+    df_cat = pd.melt(df[columns], var_name='Category', value_name='Value')
 
     # 6
-    df_cat = None
-    
+    df_cat = pd.melt(df[columns], var_name='Category', value_name='Value')
+    df_cat['Cardio'] = df['cardio'].repeat(len(df))
+    df_grouped = df_cat.groupby(['Cardio', 'Category', 'Value']).size().reset_index(name='Count')
 
     # 7
-
-
+    sns.catplot(x='Category', hue='Value', kind='count', data=df_cat)
 
     # 8
-    fig = None
-
+    fig = sns.catplot(x='Category', hue='Value', kind='count', data=df_cat)
 
     # 9
     fig.savefig('catplot.png')
     return fig
 
-
 # 10
 def draw_heat_map():
     # 11
-    df_heat = None
+    df_heat = df[(df['ap_lo'] <= df['ap_hi']) & (df['height'] >= df['height'].quantile(0.025)) &
+            (df['height'] <= df['height'].quantile(0.975)) & (df['weight'] >= df['weight'].quantile(0.025)) &
+            (df['weight'] <= df['weight'].quantile(0.975))]
 
     # 12
-    corr = None
+    corr = df_heat.corr()
 
     # 13
-    mask = None
-
-
+    mask = np.triu(np.ones_like(corr, dtype=bool))
 
     # 14
-    fig, ax = None
+    fig, ax = plt.subplots(figsize=(12, 8))
 
     # 15
-
-
+    sns.heatmap(corr, annot=True, cmap='coolwarm', fmt='.2f', mask=mask, ax=ax, cbar_kws={'shrink': 0.8})
 
     # 16
     fig.savefig('heatmap.png')
